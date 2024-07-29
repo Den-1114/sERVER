53d18ffe42624afb2e0343708c923dd4bbc3e16b
Den-1114 <119738460+Den-1114@users.noreply.github.com>
2024-07-29 15:31:19 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 import threading
-import time
 
 HEADER = 4096
-HOST = '6.tcp.eu.ngrok.io'
-PORT = 17897
+HOST = 'green-mixing.gl.at.ply.gg'
+PORT = 36217
 ADDR = (HOST, PORT)
 DISCONNECT_MESSAGE = '!DISCONNECT'
+MINECRAFT_LOCAL_IP_COMMAND = '!IP'
 FORMAT = 'utf-8'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,22 +20,26 @@ def receive():
                 print(message)
         except Exception as e:
             print(f'An error occurred: {e}\nClosing connection...')
-            print('Connection closed...')
             client.close()
-            client.send(DISCONNECT_MESSAGE.encode(FORMAT))
             break
 
-def send():
+def write():
     while True:
         message = input()
-        client.send(message.encode(FORMAT))
+        send(message)
+        if message == DISCONNECT_MESSAGE:
+            break
 
-
-send_thread = threading.Thread(target=send)
-
-send_thread.start()
-
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
 
 receive_thread = threading.Thread(target=receive)
+write_thread = threading.Thread(target=write)
 
 receive_thread.start()
+write_thread.start()
